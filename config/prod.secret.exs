f986aed2a8dad5c92489a9d996369c9b34885b7e
Serra Allgood <serra@allgood.dev>
2022-05-10 21:55:00 -0400

    Screw it
    Signed-off-by: Serra Allgood <serra@allgood.dev>

---
+++ config/prod.secret.exs
@@ -0,0 +1,47 @@
+# In this file, we load production configuration and secrets
+# from environment variables. You can also hardcode secrets,
+# although such is generally not recommended and you have to
+# remember to add this file to your .gitignore.
+use Mix.Config
+
+config :thurim, :matrix,
+  domain: System.get_env("DOMAIN") || raise("environment variable DOMAIN is missing"),
+  homeserver_url:
+    System.get_env("HOMESERVER_URL") || raise("environment variable HOMESERVER_URL is missing"),
+  identity_server_url: "https://matrix.org"
+
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
+
+config :thurim, Thurim.Repo,
+  # ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :thurim, ThurimWeb.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  secret_key_base: secret_key_base
+
+# ## Using releases (Elixir v1.9+)
+#
+# If you are doing OTP releases, you need to instruct Phoenix
+# to start each relevant endpoint:
+#
+#     config :thurim, ThurimWeb.Endpoint, server: true
+#
+# Then you can assemble a release by calling `mix release`.
+# See `mix help release` for more information.
